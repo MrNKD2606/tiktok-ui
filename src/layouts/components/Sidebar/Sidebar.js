d53e938c97fdcfc76ea434149b66ea2d4dd2948d
DuyNguyen <NKD.26061994@gmail.com>
2022-11-09 17:55:16 +0700

    update sidebar

--- src/layouts/components/Sidebar/Sidebar.js
+++ src/layouts/components/Sidebar/Sidebar.js
@@ -19,39 +19,41 @@ function Sidebar() {
 
     return (
 
-        <div className={cx('scroll-container')}>
-            <aside className={cx('wrapper')}>
-                <Menu>
-                    <MenuItem title='For you' to={config.routes.home} iconActive={<HomeIconActive />} icon={<HomeIcon />} />
-                    <MenuItem title='Following' to={config.routes.following} iconActive={<UserGroupIconActive />} icon={<UserGroupIcon />} />
-                    <MenuItem title='LIVE' to={config.routes.live} iconActive={<LiveIconActive />} icon={<LiveIcon />} />
-                </Menu>
+        <div className={cx('nav-container')}>
+            <div className={cx('scroll-container')}>
+                <aside className={cx('wrapper')}>
+                    <Menu>
+                        <MenuItem title='For you' to={config.routes.home} iconActive={<HomeIconActive />} icon={<HomeIcon />} />
+                        <MenuItem title='Following' to={config.routes.following} iconActive={<UserGroupIconActive />} icon={<UserGroupIcon />} />
+                        <MenuItem title='LIVE' to={config.routes.live} iconActive={<LiveIconActive />} icon={<LiveIcon />} />
+                    </Menu>
 
-                {!currentUser && (
-                    <div className={cx('sidebar-login')}>
-                        <p className={cx('p-login')}>Log in to follow creators, like videos, and view comments</p>
-                        <Button className={cx('btn-login')} big outline>Log in</Button>
+                    {!currentUser && (
+                        <div className={cx('sidebar-login')}>
+                            <p className={cx('p-login')}>Log in to follow creators, like videos, and view comments</p>
+                            <Button className={cx('btn-login')} big outline>Log in</Button>
+                        </div>
+                    )}
+
+                    <PreviewContext.Provider value={true}>
+                        <SuggestedAccounts label='Suggested accounts'></SuggestedAccounts>
+                    </PreviewContext.Provider>
+
+                    {currentUser && (
+                        <SuggestedAccounts label='Following accounts'></SuggestedAccounts>
+                    )}
+
+                    <Discovers label='Discovers'></Discovers>
+
+                    <Footer></Footer>
+
+                    <div className={cx('a')}>
+                        <div className={cx('b')}>
+
+                        </div>
                     </div>
-                )}
-
-                <PreviewContext.Provider value={true}>
-                    <SuggestedAccounts label='Suggested accounts'></SuggestedAccounts>
-                </PreviewContext.Provider>
-
-                {currentUser && (
-                    <SuggestedAccounts label='Following accounts'></SuggestedAccounts>
-                )}
-
-                <Discovers label='Discovers'></Discovers>
-
-                <Footer></Footer>
-
-                <div className={cx('a')}>
-                    <div className={cx('b')}>
-
-                    </div>
-                </div>
-            </aside>
+                </aside>
+            </div>
         </div>
     );
 }
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,17 @@
+import classNames from "classnames/bind";
+
+
+import styles from './Home.module.scss'
+
+const cx = classNames.bind(styles)
+
 function Home() {
-    return (<h2 style={{ height: 2000 }}> HomePage</h2 >);
+    return (
+        <div className={cx('wrapper')}>
+            {/* <Video /> */}
+            <h2 style={{ height: 2000 }}> HomePage</h2>
+        </div>
+    );
 }
 
 export default Home;
