453fa99407be199bec801a630ae60fa67e6a4da4
DuyNguyen <NKD.26061994@gmail.com>
2022-11-17 10:49:37 +0700

    update video

--- src/layouts/components/Video/Video.js
+++ src/layouts/components/Video/Video.js
@@ -1,12 +1,127 @@
 import classNames from "classnames/bind";
+import { useEffect, useRef, useState } from "react";
+import { Link } from "react-router-dom";
+import Button from "~/components/Button";
+import { CommentIcon, FlagIcon, HeartIcon, MusicIcon, MutedIcon, PauseIcon, PlayIcon, ShareIcon, VolumeIcon } from "~/components/Icons";
+import Image from "~/components/Image";
 
 import styles from './Video.module.scss'
 
 const cx = classNames.bind(styles)
 
-function Video() {
+function Video({ data, mute, volume, adjustVolume, toggleMuted }) {
+
+    const [isPlaying, setIsPlaying] = useState(false)
+    const videoRef = useRef()
+
+    useEffect(() => {
+        if (mute) {
+            videoRef.current.volume = 0
+        } else {
+            videoRef.current.volume = volume
+        }
+    })
+    const playVideo = () => {
+        if (isPlaying === false) {
+            videoRef.current.play()
+            setIsPlaying(true)
+        }
+    }
+    const pauseVideo = () => {
+        if (isPlaying === true) {
+            videoRef.current.pause()
+            setIsPlaying(false)
+        }
+    }
+
+    const togglePlayVideo = () => {
+        if (isPlaying === false) {
+            playVideo()
+        } else {
+            pauseVideo()
+        }
+    }
+
     return (
-        null
+        <div className={cx('wrapper')}>
+            <Link className={cx('avatar-anchor')}>
+                <Image className={cx('image')} src={data.user.avatar} alt={data.user.avatar} />
+            </Link>
+
+            <div className={cx('content')}>
+                <div className={cx('text-info')}>
+                    <Link className={cx('author-container')}>
+                        <h3 className={cx('author-unique')}>{data.user.nickname}</h3>
+                        <h4 className={cx('author-nickname')}>{data.user.first_name + ' ' + data.user.last_name}</h4>
+                    </Link>
+                    <div className={cx('video-text')}>
+                        {data.description}
+                    </div>
+                    {/* <Link>
+                        <strong className={cx('tag')}>#xuhuong</strong>
+                    </Link> */}
+                    <h4 className={cx('music')}>
+                        <Link>
+                            <MusicIcon className={cx('icon')} />
+                            <span className={cx('text-icon')}>{data.music}</span>
+                        </Link>
+                    </h4>
+                    <Button outline className={cx('button-wrapper')}>Follow</Button>
+                </div>
+
+                <div className={cx('video-wrapper')}>
+                    <div className={cx('feed-video')}>
+                        <video
+                            loop
+                            src={data?.file_url}
+                            ref={videoRef}>
+                        </video>
+
+                        <div className={cx('control-play')}
+                            onClick={togglePlayVideo}
+                        >
+                            {isPlaying ? <PauseIcon /> : <PlayIcon />}
+                        </div>
+
+                        <div className={cx('control-volume')}>
+                            <div className={cx('container')}>
+                                <input type="range" min="0" max="100" step="1" orient="vertical" onChange={adjustVolume} value={volume * 100} />
+                            </div>
+                            <div className={cx('volume-icon')}>
+                                <VolumeIcon />
+                            </div>
+                            {/* <MutedIcon /> */}
+                        </div>
+
+                        <div className={cx('control-seekBar')}>
+                            <div className={cx('container')}>
+                                <input type="range" min="0" max="100" step="1" orient="vertical" onChange={adjustVolume} value={volume * 100} />
+                            </div>
+                            <div className={cx('time')}>01:23/01:26</div>
+                        </div>
+
+                        <p className={cx('report')}>
+                            <FlagIcon className={cx('flag-icon')} />Report
+                        </p>
+                    </div>
+
+                    <div className={cx('action')}>
+                        <button className={cx('action-btn')}>
+                            <span className={cx('action-icon')}><HeartIcon /></span>
+                            <strong className={cx('strong-text')}>333.6k</strong>
+                        </button>
+                        <button className={cx('action-btn')}>
+                            <span className={cx('action-icon')}><CommentIcon /></span>
+                            <strong className={cx('strong-text')}>333.6k</strong>
+                        </button>
+                        <button className={cx('action-btn')}>
+                            <span className={cx('action-icon')}><ShareIcon /></span>
+                            <strong className={cx('strong-text')}>333.6k</strong>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,4 +1,9 @@
 import classNames from "classnames/bind";
+import { useEffect, useState } from "react";
+
+
+import Video from "~/layouts/components/Video";
+import * as videoService from '~/services/videoService'
 
 
 import styles from './Home.module.scss'
@@ -6,10 +11,28 @@ import styles from './Home.module.scss'
 const cx = classNames.bind(styles)
 
 function Home() {
+
+    const [videos, setVideos] = useState([])
+    const [page, setPage] = useState(1)
+    const [volume, setVolume] = useState(0.5)
+
+    useEffect(() => {
+        const fetchApi = async () => {
+            const result = await videoService.loadVideo('for-you', page)
+            setVideos(prev => [...prev, ...result])
+        }
+        fetchApi()
+    }, [page])
+
+    const handleAdjustVolume = (e) => {
+        setVolume(e.target.value / 100)
+    }
+
     return (
         <div className={cx('wrapper')}>
-            {/* <Video /> */}
-            <h2 style={{ height: 2000 }}> HomePage</h2>
+            {videos.map((video, index) => (
+                <Video key={index} data={video} volume={volume} adjustVolume={handleAdjustVolume} />
+            ))}
         </div>
     );
 }
